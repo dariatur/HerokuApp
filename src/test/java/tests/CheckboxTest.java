7bd14394bc3c0ab99fcf0526254788bdfa7ac4f2
dariatur <dashatur100500@gmail.com>
2025-02-05 16:49:14 +0100

    add test for checkboxes

--- src/test/java/tests/CheckboxTest.java
+++ src/test/java/tests/CheckboxTest.java
@@ -1,16 +1,44 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CheckboxTest {
+    private WebDriver driver = new ChromeDriver();
+
+    {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+    }
 
     @Test
-    public void checkboxTest(){
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+    public void firstCheckboxTest(){
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
 
+        WebElement firstCheckbox = driver.findElements(By.cssSelector("[type=checkbox]")).get(0);
+
+        Assert.assertEquals(firstCheckbox.isSelected(), false);
+
+        if (!firstCheckbox.isSelected()){
+            firstCheckbox.click();
+        }
+
+        Assert.assertEquals(firstCheckbox.isSelected(), true);
     }
+    @Test
+    public void secondCheckboxTest(){
+        WebElement secondCheckbox = driver.findElements(By.cssSelector("[type=checkbox]")).get(1);
+
+        Assert.assertEquals(secondCheckbox.isSelected(), true);
+
+        if (secondCheckbox.isSelected()){
+            secondCheckbox.click();
+        }
+
+        Assert.assertEquals(secondCheckbox.isSelected(), false);
+    }
+
 }
