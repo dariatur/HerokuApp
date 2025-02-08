d0402ca6c57450dcf14c08384ab00966086511b4
dariatur <dashatur100500@gmail.com>
2025-02-08 19:37:31 +0100

    git add tests for hovers

---
+++ src/test/java/tests/NotificationTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NotificationTest {
+
+    public void notificationTest(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/notification_message_rendered");
+
+        WebElement element = driver.findElement(By.tagName("a"));
+
+
+
+    }
+}
