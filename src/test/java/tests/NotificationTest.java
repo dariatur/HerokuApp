53ac12801be86eb69a5d74bb99678a1c32d784ed
dariatur <dashatur100500@gmail.com>
2025-02-09 09:02:45 +0100

    add test for notification page

--- src/test/java/tests/NotificationTest.java
+++ src/test/java/tests/NotificationTest.java
@@ -2,19 +2,24 @@ package tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class NotificationTest {
 
+    @Test
     public void notificationTest(){
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/notification_message_rendered");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        WebElement element = driver.findElement(By.tagName("a"));
+        driver.findElement(By.xpath("//*[@href='/notification_message']")).click();
+        String messageText = driver.findElement(By.id("flash-messages")).getText().split("\n")[0];
 
-
-
+        Assert.assertEquals(messageText, "Action successful");
     }
 }
