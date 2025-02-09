b2fa7fd663109c2e87ff375f49a98db23bbad5ee
dariatur <dashatur100500@gmail.com>
2025-02-09 09:07:31 +0100

    add test for inputs

---
+++ src/test/java/tests/InputTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class InputTest {
+
+    @Test
+    public  void herokuAppTest() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1024, 768));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("https://the-internet.herokuapp.com/inputs");
+        WebElement inputField = driver.findElement(By.xpath("//*[@type=\"number\"]"));
+        inputField.sendKeys("20");
+        String actualText = inputField.getAttribute("value");
+        Assert.assertEquals(actualText, "20");
+
+        inputField.sendKeys(Keys.ARROW_UP);
+        String actualText2 = inputField.getAttribute("value");
+        Assert.assertEquals(actualText2, "21");
+    }
+}
